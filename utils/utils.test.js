ee07c1408cdcaa64a21beefa0a8b1ec713535271
davidhenley <henleydr@gmail.com>
2017-03-13 11:16:22 -0500

    Basic tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,6 +1,15 @@
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  const res = utils.add(33, 11);
-  if (res !== 44) throw new Error(`Expected 44, but got ${res}.`);
+describe('Utils', () => {
+
+  it('should add two numbers', () => {
+    const res = utils.add(33, 11);
+    if (res !== 44) throw new Error(`Expected 44, but got ${res}.`);
+  });
+
+  it('should return square root of a number', () => {
+    const res = utils.square(5);
+    if (res !== 25) throw new Error(`Expected 25, but got ${res}.`);
+  });
+
 });
