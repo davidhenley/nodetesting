c9cc0142d97d13bfe204053707c158542674806c
davidhenley <henleydr@gmail.com>
2017-03-13 15:20:15 -0500

    Testing with Supertest and Expect

--- server/server.js
+++ server/server.js
@@ -3,7 +3,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send([
+    {name: 'Ryan Moses', age: 31},
+    {name: 'Kaylin Long', age: 28},
+    {name: 'David Henley', age: 32},
+  ]);
 });
 
 app.listen(3000, () => {
--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,26 @@
 const request = require('supertest');
+const expect = require('expect');
+
 const { app } = require('./server');
 
 describe('Server', () => {
   it('should return hello world', (done) => {
     request(app).get('/')
-      .expect('Hello world!')
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          error: 'Page not found.'
+        });
+      })
+      .end(done);
+  });
+
+  it('should contain a specific user', (done) => {
+    request(app).get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toInclude({ name: 'David Henley', age: 32 });
+      })
       .end(done);
   });
 });
